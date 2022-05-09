d6e91d7ecb9ade9f83f2987f05773b3db10ded64
Roshan Madhave <Roshan.Madhave@BRILLIO.COM>
2022-05-09 15:41:48 +0530

    running

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,28 @@
+const Home = () => {
+
+    return(
+
+        <div>
+            <br /><br />
+
+            <h1 className="text-center">Welcome To The Safety Zone</h1><br></br>
+            <h1 className="text-center">Lets Gets Vaccinated</h1>
+            <br></br>
+
+
+            <h6> 1 :-Stay at home as much as possible. <br></br>
+                 2 :-Avoid physical contact like handshakes, hand holding or hugs.<br></br> 
+                 3 :-Avoid touching surfaces such as table tops, chairs, door handles etc.
+                </h6><br></br>
+
+<label> Click Here  For Safety Precautions:</label><br></br>
+                <a href="url">https://www.who.int/westernpacific/emergencies/covid-19/information/transmission-protective-measures</a>
+     
+
+        </div>
+
+    )
+
+}
+
+export default Home;
